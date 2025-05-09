e7379354d6a78aabe1ccedecf3fb7782d9b0821e
synka777 <m.barbegayet@gmail.com>
2025-05-09 18:34:34 +0200

    infra(sidebar): +material logos

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
+import { MatIcon } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-main-layout',
-  imports: [ RouterOutlet ],
+  imports: [ RouterOutlet, MatIcon ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
