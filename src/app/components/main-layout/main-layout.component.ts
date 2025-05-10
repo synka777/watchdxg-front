11d792a178f6b6324a5a2bd66fbd45a83e4778c5
synka777 <m.barbegayet@gmail.com>
2025-05-10 18:59:53 +0200

    infra(sidebar): +collapse prefs persist. +logout button

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,8 +1,9 @@
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,21 +12,47 @@ import { Component } from '@angular/core';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit{
   isCollapsed: boolean = false
+  isScreenSmall: boolean = false
 
-  constructor() {
-    this.checkViewport()
-    window.addEventListener('resize', () => {
-      this.checkViewport()
-    })
+  private _userPrefersCollapse!: boolean
+
+  constructor (
+    private _router: Router,
+    private _breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit(): void {
+    // Get the user's sidebar preference from localStorage
+    this._userPrefersCollapse = localStorage.getItem('sidebarCollapsed') === 'true';
+
+    // Observe changes in screen size (auto collapse when small screen)
+    this._breakpointObserver
+    .observe(['(max-width: 768px)'])
+    .subscribe(result => {
+      this.isScreenSmall = result.matches;
+      this.updateCollapseState();
+    });
+
+    // Ensure the collapse state is set initially
+    this.updateCollapseState();
   }
 
+  // Toggle the sidebar collapse and store preference in localStorage
   toggleSidebar() {
-    this.isCollapsed = !this.isCollapsed
+    this._userPrefersCollapse = !this._userPrefersCollapse
+    localStorage.setItem('sidebarCollapsed', String(this._userPrefersCollapse))
+    this.updateCollapseState()
   }
 
-  checkViewport() {
-    this.isCollapsed = window.innerWidth < 768
+  // Update collapse state based on screen size or user preference
+  updateCollapseState() {
+    this.isCollapsed = this.isScreenSmall || this._userPrefersCollapse
+  }
+
+  logout() {
+    localStorage.removeItem('login')
+    this._router.navigate(['/login'])
   }
 }
