ce287267b43a01d369ccd7e85875a8b8932daf60
synka777 <m.barbegayet@gmail.com>
2025-05-11 18:53:17 +0200

    misc(name): renamed project for more clarity

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'watchdxg-gui' title`, () => {
+  it(`should have the 'watchdxg-front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('watchdxg-gui');
+    expect(app.title).toEqual('watchdxg-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, watchdxg-gui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, watchdxg-front');
   });
 });
